0a8d9b8722cf0604863cda6090a94b014d4b2ab1
izidormaklary <maklaryizidor@gmail.com>
2021-08-05 14:38:44 +0200

    login created, todo:registration

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Embedded;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=StudentRepository::class)
